d1c5f2aadef74e6af7b1792b3b2c2e90b14b283b
GooJinSun <gjinsun96@gmail.com>
2022-07-16 17:41:47 +0900

    feat: #10 make access token lifetime 1 hour

--- backend/adoorback/adoorback/settings/base.py
+++ backend/adoorback/adoorback/settings/base.py
@@ -38,7 +38,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
