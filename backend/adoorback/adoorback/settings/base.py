015271ff28f3d89b0e2f0de4791f1c0dc82ce652
GooJinSun <gjinsun96@gmail.com>
2022-07-24 21:28:47 +0900

    bugfix: #10 remove ROTATE_REFRESH_TOKEN options from simple-jwt settings

--- backend/adoorback/adoorback/settings/base.py
+++ backend/adoorback/adoorback/settings/base.py
@@ -40,7 +40,6 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
-    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 
     'AUTH_HEADER_TYPES': ('Bearer',),
