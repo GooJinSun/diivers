e0b98872f06468e51ee64ea5384134788ed19e36
GooJinSun <gjinsun96@gmail.com>
2022-07-30 19:22:00 +0900

    feat: #10 add ROTATE_REFRESH_TOKENS option to SIMPLE_JWT & reduce RFRESH_TOKEN_LIFETIME as 30 days to improve token security

--- backend/adoorback/adoorback/settings/base.py
+++ backend/adoorback/adoorback/settings/base.py
@@ -39,8 +39,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
-    # ROTATE_REFRESH_TOKENS
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 
     'AUTH_HEADER_TYPES': ('Bearer',),
