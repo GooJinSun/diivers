a7d29bbaf0b862b5c24065911f48553b92b392dd
JaeWon Kim <25769209+ssm0318@users.noreply.github.com>
2022-07-30 15:44:55 +0900

    fix: #30 remove unnecessary code

--- backend/adoorback/adoorback/settings/base.py
+++ backend/adoorback/adoorback/settings/base.py
@@ -30,7 +30,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 15,
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        # 'rest_framework.authentication.SessionAuthentication',
         'adoorback.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
