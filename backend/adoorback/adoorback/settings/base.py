1f46eae8f69ece273a009494da32e5ccea856bab
ssm0318 <slove0318@snu.ac.kr>
2022-09-18 18:51:34 -0700

    modify env for django settings

--- backend/adoorback/adoorback/settings/base.py
+++ backend/adoorback/adoorback/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 import datetime
 import os.path
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '((5+vsn)pefc7()9_x2oud)po=@0=@gf0=8j)lrk*(*sy47c+='
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
