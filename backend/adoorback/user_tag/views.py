fc058335c03eee5a2d3acd1d40416d5276c94616
Yuri Kim <njs03332@gmail.com>
2022-10-23 20:06:42 +0900

    user tag search : icontains -> startswith + icontains

--- backend/adoorback/user_tag/views.py
+++ backend/adoorback/user_tag/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
+from django.db.models import Case, Value, When, IntegerField
 
 from user_tag.models import UserTag
 from account.serializers import UserFriendshipStatusSerializer
@@ -27,8 +28,19 @@ class UserTagSearch(generics.ListAPIView):
     def get_queryset(self):
         username = self.request.GET.get('query')
         user = self.request.user
-        queryset = User.objects.none()
+        qs = User.objects.none()
         if username:
-            queryset = user.friends.filter(
-                username__icontains=self.request.GET.get('query')).order_by('username')[:10]
-        return queryset
+            qs_ids = list(user.friends.filter(
+                username__startswith=username).order_by('username')[:10].values_list('id', flat=True))
+            if len(qs_ids) < 10:
+                contain_ids = list(user.friends.filter(
+                        username__icontains=username).order_by('username').values_list('id', flat=True))
+                for id_ in contain_ids:
+                    if id_ not in qs_ids:
+                        qs_ids.append(id_)
+            # merge querysets while preserving order
+            cases = [When(id=x, then=Value(i)) for i,x in enumerate(qs_ids)] 
+            case = Case(*cases, output_field=IntegerField())
+            qs = User.objects.filter(id__in=qs_ids).annotate(my_order=case).order_by('my_order')
+
+        return qs
