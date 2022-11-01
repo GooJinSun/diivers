c45d767467230652be0245444f1b293a8de70aee
Hyesoo KIM <hyesoo5115@naver.com>
2022-11-01 20:33:04 +0900

    fix: #43 fix wrap_content util function to shorten content up to 18 characters

--- backend/adoorback/utils/wrap_content.py
+++ backend/adoorback/utils/wrap_content.py
@@ -3,9 +3,7 @@ import textwrap
 
 def wrap_content(content):
     content_as_string = str(content)
-    words_in_content = content_as_string.split()
-    first_word = words_in_content[0]
-
-    if len(first_word) > 15:
-        return f'{first_word}...' if len(words_in_content) > 1 else first_word
-    return textwrap.shorten(content_as_string, width=18, placeholder='...')
+    wrap_result = textwrap.shorten(content_as_string, width=18, placeholder='...')
+    if wrap_result != '...':
+        return wrap_result
+    return f'{content_as_string[:15].rstrip()}...'
