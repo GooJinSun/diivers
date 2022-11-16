42461de0a499eb544187d9d0e6785bdb0b272f02
GooJinSun <gjinsun96@gmail.com>
2022-11-16 22:14:13 +0900

    feat: #3 add redux state, actions for friendRequest, responseRequest type notification

--- frontend/src/modules/notification.js
+++ frontend/src/modules/notification.js
@@ -2,6 +2,8 @@ import axios from '../apis';
 
 const initialState = {
   receivedNotifications: [],
+  receivedFriendRequests: [],
+  receivedResponseRequests: [],
   next: null
 };
 
@@ -19,6 +21,34 @@ export const APPEND_NOTIFICATIONS_SUCCESS =
 export const APPEND_NOTIFICATIONS_FAILURE =
   'notification/APPEND_NOTIFICATION_FAILURE';
 
+export const GET_FRIEND_REQUESTS_REQUEST =
+  'notification/GET_FRIEND_REQUESTS_REQUEST';
+export const GET_FRIEND_REQUESTS_SUCCESS =
+  'FRIEND_REQUESTS/GET_FRIEND_REQUESTS_SUCCESS';
+export const GET_FRIEND_REQUESTS_FAILURE =
+  'FRIEND_REQUESTS/GET_FRIEND_REQUESTS_FAILURE';
+
+export const APPEND_FRIEND_REQUESTS_REQUEST =
+  'FRIEND_REQUESTS/APPEND_FRIEND_REQUESTS_REQUEST';
+export const APPEND_FRIEND_REQUESTS_SUCCESS =
+  'FRIEND_REQUESTS/APPEND_FRIEND_REQUESTS_SUCCESS';
+export const APPEND_FRIEND_REQUESTS_FAILURE =
+  'FRIEND_REQUESTS/APPEND_FRIEND_REQUESTS_FAILURE';
+
+export const GET_RESPONSE_REQUESTS_REQUEST =
+  'notification/GET_RESPONSE_REQUESTS_REQUEST';
+export const GET_RESPONSE_REQUESTS_SUCCESS =
+  'RESPONSE_REQUESTS/GET_RESPONSE_REQUESTS_SUCCESS';
+export const GET_RESPONSE_REQUESTS_FAILURE =
+  'RESPONSE_REQUESTS/GET_RESPONSE_REQUESTS_FAILURE';
+
+export const APPEND_RESPONSE_REQUESTS_REQUEST =
+  'RESPONSE_REQUESTS/APPEND_RESPONSE_REQUESTS_REQUEST';
+export const APPEND_RESPONSE_REQUESTS_SUCCESS =
+  'RESPONSE_REQUESTS/APPEND_RESPONSE_REQUESTS_SUCCESS';
+export const APPEND_RESPONSE_REQUESTS_FAILURE =
+  'RESPONSE_REQUESTS/APPEND_NOTIFICATION_FAILURE';
+
 export const READ_NOTIFICATION_REQUEST =
   'notification/READ_NOTIFICATION_REQUEST';
 export const READ_NOTIFICATION_SUCCESS =
@@ -33,22 +63,20 @@ export const READ_ALL_NOTIFICATIONS_SUCCESS =
 export const READ_ALL_NOTIFICATIONS_FAILURE =
   'notification/READ_ALL_NOTIFICATIONS_FAILURE';
 
-export const DELETE_NOTIFICATION_REQUEST = '';
-
 export const getNotifications = () => async (dispatch) => {
   let res;
-  dispatch({ type: 'notification/GET_NOTIFICATION_REQUEST' });
+  dispatch({ type: GET_NOTIFICATIONS_REQUEST });
   try {
     res = await axios.get('/notifications/');
   } catch (error) {
     dispatch({
-      type: 'notification/GET_NOTIFICATION_FAILURE',
+      type: GET_NOTIFICATIONS_FAILURE,
       error
     });
     return;
   }
   dispatch({
-    type: 'notification/GET_NOTIFICATION_SUCCESS',
+    type: GET_NOTIFICATIONS_SUCCESS,
     res: res?.data.results,
     next: res?.data.next
   });
@@ -69,6 +97,84 @@ export const appendNotifications = () => async (dispatch, getState) => {
   const { data } = result;
   dispatch({
     type: APPEND_NOTIFICATIONS_SUCCESS,
+    results: data?.results,
+    next: data?.next
+  });
+};
+
+export const getFriendRequests = () => async (dispatch) => {
+  let res;
+  dispatch({ type: GET_FRIEND_REQUESTS_REQUEST });
+  try {
+    res = await axios.get('/notifications/friend-requests/');
+  } catch (error) {
+    dispatch({
+      type: GET_FRIEND_REQUESTS_FAILURE,
+      error
+    });
+    return;
+  }
+  dispatch({
+    type: GET_FRIEND_REQUESTS_SUCCESS,
+    res: res?.data.results,
+    next: res?.data.next
+  });
+};
+
+export const appendFriendRequests = () => async (dispatch, getState) => {
+  const { next } = getState().notiReducer;
+  if (!next) return;
+  const nextUrl = next.replace('http://localhost:8000/api/', '');
+  let result;
+  dispatch({ type: APPEND_FRIEND_REQUESTS_REQUEST });
+  try {
+    result = await axios.get(nextUrl);
+  } catch (error) {
+    dispatch({ type: APPEND_FRIEND_REQUESTS_FAILURE, error });
+    return;
+  }
+  const { data } = result;
+  dispatch({
+    type: APPEND_FRIEND_REQUESTS_SUCCESS,
+    results: data?.results,
+    next: data?.next
+  });
+};
+
+export const getResponseRequests = () => async (dispatch) => {
+  let res;
+  dispatch({ type: GET_RESPONSE_REQUESTS_REQUEST });
+  try {
+    res = await axios.get('/notifications/response-requests/');
+  } catch (error) {
+    dispatch({
+      type: GET_RESPONSE_REQUESTS_FAILURE,
+      error
+    });
+    return;
+  }
+  dispatch({
+    type: GET_RESPONSE_REQUESTS_SUCCESS,
+    res: res?.data.results,
+    next: res?.data.next
+  });
+};
+
+export const appendResponseRequests = () => async (dispatch, getState) => {
+  const { next } = getState().notiReducer;
+  if (!next) return;
+  const nextUrl = next.replace('http://localhost:8000/api/', '');
+  let result;
+  dispatch({ type: APPEND_RESPONSE_REQUESTS_REQUEST });
+  try {
+    result = await axios.get(nextUrl);
+  } catch (error) {
+    dispatch({ type: APPEND_RESPONSE_REQUESTS_FAILURE, error });
+    return;
+  }
+  const { data } = result;
+  dispatch({
+    type: APPEND_RESPONSE_REQUESTS_SUCCESS,
     results: data?.results,
     next: data?.next
   });
@@ -137,6 +243,34 @@ export default function notiReducer(state, action) {
         ...state,
         receivedNotifications: state.receivedNotifications
           ? [...state.receivedNotifications, ...action.results]
+          : [...action.results],
+        next: action.next
+      };
+    case GET_FRIEND_REQUESTS_SUCCESS:
+      return {
+        ...state,
+        receivedFriendRequests: action.res,
+        next: action.next
+      };
+    case APPEND_FRIEND_REQUESTS_SUCCESS:
+      return {
+        ...state,
+        receivedFriendRequests: state.receivedFriendRequests
+          ? [...state.receivedFriendRequests, ...action.results]
+          : [...action.results],
+        next: action.next
+      };
+    case GET_RESPONSE_REQUESTS_SUCCESS:
+      return {
+        ...state,
+        receivedResponseRequests: action.res,
+        next: action.next
+      };
+    case APPEND_RESPONSE_REQUESTS_SUCCESS:
+      return {
+        ...state,
+        receivedResponseRequests: state.receivedResponseRequests
+          ? [...state.receivedResponseRequests, ...action.results]
           : [...action.results],
         next: action.next
       };
