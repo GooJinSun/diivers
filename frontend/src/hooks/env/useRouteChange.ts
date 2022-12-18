4b87a775f214a4e4b0b70364107bf7ccc0c132c7
koyrkr <koyrkr@gmail.com>
2022-12-19 01:21:17 +0900

    fix: change useRouteChange file from js to ts

--- frontend/src/hooks/env/useRouteChange.ts
+++ frontend/src/hooks/env/useRouteChange.ts
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useHistory } from 'react-router';
 
-const useRouteChange = (onRouteChangeCallback) => {
+const useRouteChange = (onRouteChangeCallback: () => void) => {
   const history = useHistory();
 
   useEffect(() => {
