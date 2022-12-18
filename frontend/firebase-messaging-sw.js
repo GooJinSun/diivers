18d2d624d5c285d84aac7df2ec43a2db554d3a43
GooJinSun <gjinsun96@gmail.com>
2022-12-18 15:41:42 +0900

    feat: #44 add goto noti url on click background fcm message

--- frontend/firebase-messaging-sw.js
+++ frontend/firebase-messaging-sw.js
@@ -2,7 +2,7 @@
  * 서비스 워커 설정 파일
  * 웹앱에서 백그라운드 노티를 수신할 때 동작합니다
  */
-
+/* eslint-disable no-restricted-globals */
 /* eslint-disable no-undef */
 // Scripts for firebase and firebase messaging
 // 참고: 왜 importScripts를 사용하나요? - https://web.dev/es-modules-in-sw/
@@ -27,6 +27,15 @@ const firebaseApp = firebase.initializeApp({
   // databaseURL: 'https://project-id.firebaseio.com',
   // storageBucket: 'project-id.appspot.com',
   // measurementId: 'G-measurement-id'
+});
+
+// NOTE: Most importantly, in your service worker add a 'notificationclick' event listener before calling firebase.messaging()
+self.addEventListener('notificationclick', (e) => {
+  e.stopImmediatePropagation();
+  e.waitUntil(
+    clients.openWindow(`${self.origin}${e.notification.data.FCM_MSG.data.url}`)
+  );
+  e.notification.close();
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background messages.
