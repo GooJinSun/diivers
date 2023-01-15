0f3c15ca2826dfdcaf89017b8929b48697edc8d5
GooJinSun <gjinsun96@gmail.com>
2023-01-15 15:25:18 +0900

    feat: #132 add account acitvate page langs

--- frontend/src/components/account-activate/AccountActivate.jsx
+++ frontend/src/components/account-activate/AccountActivate.jsx
@@ -3,14 +3,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import { requestActivate } from '@modules/user';
 import { AuthentiCationWrapper } from '@styles/wrappers';
+import { useTranslation } from 'react-i18next';
 
 export default function AccountActivate() {
   const dispatch = useDispatch();
   const { id, token } = useParams();
 
+  const [t] = useTranslation('translation', { keyPrefix: 'account_activate' });
+
   useEffect(() => {
     dispatch(requestActivate(id, token));
-  }, []);
+  }, [dispatch, id, token]);
 
   const [isActivatePending, setIsActivatePending] = useState(true);
   const [isActivateSuccess, setIsActivateSuccess] = useState(false);
@@ -27,11 +30,11 @@ export default function AccountActivate() {
   return (
     <AuthentiCationWrapper>
       {isActivatePending ? (
-        <div>인증 중 ... </div>
+        <div>{t('authenticating')}</div>
       ) : isActivateSuccess ? (
-        <div>이메일 인증이 완료되었습니다.</div>
+        <div>{t('email_authentication_is_completed')}</div>
       ) : (
-        <div>유효하지 않은 링크입니다.</div>
+        <div>{t('invalid_link')}</div>
       )}
     </AuthentiCationWrapper>
   );
