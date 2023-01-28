572a65c9d689f5f5944d85450bf0c0cabecce7b0
GooJinSun <gjinun96@gmail.com>
2023-01-28 21:39:41 +0900

    feat: #134 add select, date pickers for MoreAboutDiiversModal

--- frontend/src/components/_common/more-about-diivers-modal/MoreAboutDiivers.styles.ts
+++ frontend/src/components/_common/more-about-diivers-modal/MoreAboutDiivers.styles.ts
@@ -2,5 +2,11 @@ import { makeStyles } from '@material-ui/core';
 import { modalStyles } from '@styles/modal';
 
 export const useStyles = makeStyles((theme) => ({
-  ...modalStyles(theme)
+  ...modalStyles(theme),
+  select: {
+    display: 'block'
+  },
+  formControl: {
+    marginBottom: '10px'
+  }
 }));
--- frontend/src/components/_common/more-about-diivers-modal/MoreAboutDiiversModal.tsx
+++ frontend/src/components/_common/more-about-diivers-modal/MoreAboutDiiversModal.tsx
@@ -1,13 +1,22 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
 import {
   Dialog,
   DialogContent,
   DialogTitle,
-  IconButton
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select
 } from '@material-ui/core';
+import DateFnsUtils from '@date-io/date-fns';
 import CloseIcon from '@material-ui/icons/Close';
-import { CommonInput } from '@styles/inputs';
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider
+} from '@material-ui/pickers';
 import { CommonButton } from '@styles/buttons';
+import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
 import { useStyles } from './MoreAboutDiivers.styles';
 
 interface AdditionalInfo {
@@ -29,11 +38,29 @@ const MoreAboutDiiversModal = ({
 }: MoreAboutDiiversModalProps) => {
   const classes = useStyles();
 
+  const [gender, setGender] = useState<number>();
+  const [birthDate, setBirthDate] = useState<string>();
+  const [ethnicity, setEthnicity] = useState<number>();
+
+  const onChangeGender = (e: ChangeEvent<{ value: unknown }>) => {
+    setGender(Number(e.target.value));
+  };
+
+  const onChangeBirthDate = (date: MaterialUiPickersDate) => {
+    // TODO: 타입 확인 필요
+    if (!date) return;
+    setBirthDate(date.toDateString());
+  };
+
+  const onChangeEthnicity = (e: ChangeEvent<{ value: unknown }>) => {
+    setEthnicity(Number(e.target.value));
+  };
+
   const onApprove = () => {
     setAdditionalUserInfo({
-      gender: undefined,
-      date_of_birth: undefined,
-      ethnicity: undefined,
+      gender,
+      date_of_birth: birthDate,
+      ethnicity,
       research_agreement: true
     });
     handleClose();
@@ -75,14 +102,67 @@ const MoreAboutDiiversModal = ({
           연구에 동의하시는 분은 아래 추가사항을 입력 해주세요. 다시한번,
           개인정보는 암호화되며 연구 외의 목적으로 이용되지 않음을 안내드립니다.
         </div>
-
-        <CommonInput id="gender-input" name="gender" placeholder="성별" />
-        <CommonInput
-          id="date_of_birth-input"
-          name="date_of_birth"
-          placeholder="생년월일"
-        />
-        <CommonInput id="ethnicity-input" name="ethnicity" placeholder="인종" />
+        {/* 나이 */}
+        <FormControl fullWidth margin="normal">
+          <InputLabel shrink id="age">
+            나이
+          </InputLabel>
+          <Select
+            labelId="age"
+            id="age-select"
+            value={gender ?? ''}
+            onChange={(e) => onChangeGender(e)}
+          >
+            <MenuItem value={0}>여성</MenuItem>
+            <MenuItem value={1}>남성</MenuItem>
+            <MenuItem value={2}>트랜스젠더 (transgender)</MenuItem>
+            <MenuItem value={3}>
+              논바이너리 (non-binary/non-conforming)
+            </MenuItem>
+            <MenuItem value={4}>응답하고 싶지 않음</MenuItem>
+          </Select>
+        </FormControl>
+        {/* 생년월일 */}
+        <FormControl fullWidth margin="normal">
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              disableToolbar
+              format="yyyy/MM/dd"
+              id="birth-date-picker"
+              label="생년월일"
+              value={birthDate}
+              onChange={(date) => onChangeBirthDate(date)}
+            />
+          </MuiPickersUtilsProvider>
+        </FormControl>
+        {/* 인종 */}
+        <FormControl fullWidth margin="normal">
+          <InputLabel id="ethnicity" shrink>
+            인종
+          </InputLabel>
+          <Select
+            labelId="ethnicity"
+            id="ethnicity-select"
+            value={ethnicity ?? ''}
+            onChange={(e) => onChangeEthnicity(e)}
+          >
+            <MenuItem value={0}>
+              미국 원주민/알래스카 원주민 (American Indian/Alaska Native)
+            </MenuItem>
+            <MenuItem value={1}>아시아인 (Asian)</MenuItem>
+            <MenuItem value={2}>
+              흑인/아프리카계 미국인 (Black/African American)
+            </MenuItem>
+            <MenuItem value={3}>
+              히스패닉/라틴계 미국인 (Hispanic/Latino)
+            </MenuItem>
+            <MenuItem value={4}>
+              하와이 원주민/다른 태평양 섬 주민 (Native Hawaiian/Other Pacific
+              Islander)
+            </MenuItem>
+            <MenuItem value={5}>백인 (White)</MenuItem>
+          </Select>
+        </FormControl>
         <CommonButton onClick={onRefuse}>동의안함</CommonButton>
         <CommonButton onClick={onApprove}>동의함</CommonButton>
       </DialogContent>
