f1ffe709e842226ae1427cff257d5700327bc58d
koyrkr <koyrkr@gmail.com>
2023-01-28 16:34:43 +0900

    fix: fix file name

--- frontend/src/hooks/useRestoreScroll.ts
+++ frontend/src/hooks/useRestoreScroll.ts
@@ -2,10 +2,12 @@ import { RootState } from '@modules/index';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
-export const useScrollRestoration = () => {
+const useRestoreScroll = () => {
   const { scrollY } = useSelector((state: RootState) => state.scrollReducer);
 
   useEffect(() => {
     window.scrollTo({ top: scrollY });
   }, []);
 };
+
+export default useRestoreScroll;
