70ab3cdc9e3215f9bda96ab35b662f732449236e
GooJinSun <gjinsun96@gmail.com>
2023-03-18 15:56:51 +0900

    feat: #143 add response draft list

--- frontend/src/components/draft-list/DraftList.tsx
+++ frontend/src/components/draft-list/DraftList.tsx
@@ -4,13 +4,16 @@ import React from 'react';
 import { useHistory } from 'react-router';
 import CreateTime from '@common-components/create-time/CreateTime';
 import { useTranslation } from 'react-i18next';
-import { DraftItem } from './draftList.styles';
+import useResponseDraft from '@hooks/draft/useResponseDraft';
+import { QuestionBoxWrapper } from '@components/_common/question-box/QuestionBox.styles';
+import { ArticleDraftItem, ResponseDraftItem } from './draftList.styles';
 
 const DraftList = () => {
   const history = useHistory();
   const [t] = useTranslation('translation', { keyPrefix: 'draft' });
 
   const { draftList: articleDraftList } = useArticleDraft();
+  const { draftList: responseDraftList } = useResponseDraft();
 
   const onClickDraft = (id: number) => {
     history.push(`/draft/articles/${id}`);
@@ -19,19 +22,38 @@ const DraftList = () => {
   return (
     <PostListWrapper>
       <h3>{t('draft_title')}</h3>
-      {!!articleDraftList?.length &&
-        articleDraftList.map((draft) => (
-          <DraftItem
-            key={draft.id}
-            type="button"
-            onClick={() => onClickDraft(draft.id)}
-          >
-            <div className="main-contents">{draft.content}</div>
-            <div className="sub-contents">
+
+      {!!articleDraftList?.length && (
+        <>
+          <h2>{t('draft_TMI_title')}</h2>
+          {articleDraftList.map((draft) => (
+            <ArticleDraftItem
+              key={draft.id}
+              type="button"
+              onClick={() => onClickDraft(draft.id)}
+            >
+              <div className="main-contents">{draft.content}</div>
+              <div className="sub-contents">
+                <CreateTime createdTime={draft.updated_at} />
+              </div>
+            </ArticleDraftItem>
+          ))}
+        </>
+      )}
+      {!!responseDraftList?.length && (
+        <>
+          <h2>{t('draft_Q&A_title')}</h2>
+          {responseDraftList.map((draft) => (
+            <ResponseDraftItem key={draft.id}>
+              <QuestionBoxWrapper>
+                {draft.question_detail.content}
+              </QuestionBoxWrapper>
+              <div className="main-contents">{draft.content}</div>
               <CreateTime createdTime={draft.updated_at} />
-            </div>
-          </DraftItem>
-        ))}
+            </ResponseDraftItem>
+          ))}
+        </>
+      )}
     </PostListWrapper>
   );
 };
--- frontend/src/components/friend-feed/go-to-draft-button/GoToDraftButton.tsx
+++ frontend/src/components/friend-feed/go-to-draft-button/GoToDraftButton.tsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router';
 import useArticleDraft from '@hooks/draft/useArticleDraft';
 import { BorderCommonButton } from '@styles/buttons';
 import { useTranslation } from 'react-i18next';
+import useResponseDraft from '@hooks/draft/useResponseDraft';
 
 const GoToDraftButton = () => {
   const [t] = useTranslation('translation', { keyPrefix: 'draft' });
@@ -12,10 +13,16 @@ const GoToDraftButton = () => {
     history.push('/draft');
   };
 
-  const { draftList } = useArticleDraft();
+  const { draftList: articleDraftList } = useArticleDraft();
+  const { draftList: responseDraftList } = useResponseDraft();
+
+  if (!articleDraftList?.length && !responseDraftList.length) return null;
+
   return (
     <BorderCommonButton type="button" onClick={onClick} margin="0 0 16px 0">
-      {`${t('draft_title')} ${draftList.length}`}
+      {`${t('draft_title')} ${
+        articleDraftList.length + responseDraftList.length
+      }`}
     </BorderCommonButton>
   );
 };
