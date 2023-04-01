ec8a792cfe62c79e729f2f33ea1f9da2b84e0bbc
Yuri <37107740+njs03332@users.noreply.github.com>
2023-04-01 22:28:37 +0900

    (#151) [회원가입][백엔드] 비밀번호 형식 제한 추가 (#261)
    feat: #151 add password constraints & validation

--- backend/adoorback/adoorback/utils/validators.py
+++ backend/adoorback/adoorback/utils/validators.py
@@ -1,6 +1,8 @@
 # import sentry_sdk
+import re
 
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 # from rest_framework import status
 from rest_framework.views import exception_handler
@@ -26,3 +28,62 @@ def adoor_exception_handler(e, context):
     #                             status.HTTP_403_FORBIDDEN]:
     # sentry_sdk.capture_exception(e)
     return response
+
+
+class NumberValidator(object):
+    def validate(self, password, user=None):
+        if not re.findall('\d', password):
+            raise ValidationError(
+                _("비밀번호는 숫자(0-9)를 한 개 이상 포함해야 합니다."),
+                code='password_no_number',
+            )
+
+    def get_help_text(self):
+        return _(
+            "비밀번호는 숫자(0-9)를 한 개 이상 포함해야 합니다."
+        )
+    
+
+class UppercaseValidator(object):
+    def validate(self, password, user=None):
+        if not re.findall('[A-Z]', password):
+            raise ValidationError(
+                _("비밀번호는 알파벳 대문자(A-Z)를 한 개 이상 포함해야 합니다."),
+                code='password_no_upper',
+            )
+
+    def get_help_text(self):
+        return _(
+            "비밀번호는 알파벳 대문자(A-Z)를 한 개 이상 포함해야 합니다."
+        )
+
+
+class LowercaseValidator(object):
+    def validate(self, password, user=None):
+        if not re.findall('[a-z]', password):
+            raise ValidationError(
+                _("비밀번호는 알파벳 소문자(a-z)를 한 개 이상 포함해야 합니다."),
+                code='password_no_lower',
+            )
+
+    def get_help_text(self):
+        return _(
+            "비밀번호는 알파벳 소문자(a-z)를 한 개 이상 포함해야 합니다."
+        )
+
+
+class SymbolValidator(object):
+    def validate(self, password, user=None):
+        if not re.findall('[()[\]{}|\\`~!@#$%^&*_\-+=;:\'",<>./?]', password):
+            raise ValidationError(
+                _("비밀번호는 특수문자를 한 개 이상 포함해야 합니다: " +
+                  "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?"),
+                code='password_no_symbol',
+            )
+
+    def get_help_text(self):
+        return _(
+            "비밀번호는 특수문자를 한 개 이상 포함해야 합니다: " +
+            "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?"
+        )
+    
