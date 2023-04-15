12682651dd58a32b699080c36da4e24167310645
Yuri Kim <njs03332@gmail.com>
2023-04-15 16:45:20 +0900

    feat: #22 fix user tag offset bug

--- backend/adoorback/utils/helpers.py
+++ backend/adoorback/utils/helpers.py
@@ -1,3 +1,4 @@
+from itertools import chain
 import re
 
 from django.contrib.auth import get_user_model
@@ -21,7 +22,7 @@ def parse_user_tag_from_content(content):
         # cut username by regex (exclude unallowed characters)
         tagged_username = re.compile(USERNAME_REGEX[1:-2]).match(word[1:]).group()
         try:
-            tagged_users |= User.objects.filter(username=tagged_username)
+            tagged_users = list(chain(tagged_users, User.objects.filter(username=tagged_username)))
             word_indices.append(i)
         except User.DoesNotExist:
             continue
