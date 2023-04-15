d14c8a8b22b2c150141033558ed5c2bce88df6ae
GooJinSun <gjinsun96@gmail.com>
2023-04-15 15:21:04 +0900

    refact: #201 set default userAgent

--- frontend/src/utils/getUserAgent.ts
+++ frontend/src/utils/getUserAgent.ts
@@ -1,4 +1,4 @@
-export const isMac = (userAgent?: string) => {
+export const isMac = (userAgent = window?.navigator.userAgent) => {
   if (!userAgent) return false;
   return userAgent.indexOf('Mac') !== -1;
 };
