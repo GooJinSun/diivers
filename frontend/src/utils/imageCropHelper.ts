f5bb22ea0bc8e30a3e390611f31ce2d26680f581
koyrkr <koyrkr@gmail.com>
2023-04-23 00:45:02 +0900

    fix: fix cropAndResize logic for width long pic

--- frontend/src/utils/imageCropHelper.ts
+++ frontend/src/utils/imageCropHelper.ts
@@ -7,21 +7,19 @@ export const cropAndResize = (file: File) =>
       const canvas = document.createElement('canvas');
       const ctx = canvas.getContext('2d');
 
-      // 캔버스 크기 설정 및 이미지 중앙에 위치시키기
+      // 이미지의 가로세로 비율(ratio)을 계산하여, 크롭할 영역 계산
       const ratio = Math.min(400 / img.width, 400 / img.height);
-      canvas.width = img.width * ratio;
-      canvas.height = img.height * ratio;
-      ctx?.drawImage(
-        img,
-        0,
-        0,
-        img.width,
-        img.height,
-        (canvas.width - img.width * ratio) / 2,
-        (canvas.height - img.height * ratio) / 2,
-        img.width * ratio,
-        img.height * ratio
-      );
+      const cropWidth = img.width * ratio;
+      const cropHeight = img.height * ratio;
+      const cropX = (img.width - cropWidth) / 2;
+      const cropY = (img.height - cropHeight) / 2;
+
+      // 캔버스 크기 설정
+      canvas.width = 400;
+      canvas.height = 400;
+
+      // 이미지 중앙에 위치시키기
+      ctx?.drawImage(img, cropX, cropY, cropWidth, cropHeight, 0, 0, 400, 400);
 
       // 변환된 Blob 객체 반환
       canvas.toBlob((blob) => {
